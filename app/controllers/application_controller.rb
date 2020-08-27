4127dd3333c898b45337c79f24f18550971d1641
shtsukada <st01251988@gmail.com>
2020-08-27 17:46:09 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+     username == ENV["FURIMA_BASIC_AUTH_USER"] && password == ENV["FURIMA_BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
 end
