c4f90d7f8c252adc7b6d2aa9a7d1089d4cba2227
shtsukada <st01251988@gmail.com>
2020-08-30 15:27:26 +0900

    レビューに対する修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,3 @@
 class UsersController < ApplicationController
   def index; end
-
-  # def show
-  # end
-
-  # def new
-  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email
-    validates :password, format: { with: /\A[a-z0-9]+\z/ }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
