54245bb15e1d644084e3c78c9b0afe8f5456878c
shtsukada <st01251988@gmail.com>
2020-08-30 14:28:50 +0900

    rubocopによる修整

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,11 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email
-    validates :password, format: { with: /\A[a-z0-9]+\z/}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :password, format: { with: /\A[a-z0-9]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
-
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {"hoge28828"}#Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {"あ井ウ"}
-    first_name            {"え尾カ"}
-    last_name_kana        {"アイウ"}
-    first_name_kana       {"エオカ"}
-    birthday              {"1930-01-01"}
-
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'hoge28828' } # Faker::Internet.password(min_length: 6)}
+    password_confirmation { password }
+    last_name             { 'あ井ウ' }
+    first_name            { 'え尾カ' }
+    last_name_kana        { 'アイウ' }
+    first_name_kana       { 'エオカ' }
+    birthday              { '1930-01-01' }
   end
 end
