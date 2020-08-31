41c5e8e9eaec8204bb3f9659f0532688077365f1
shtsukada <st01251988@gmail.com>
2020-08-31 19:17:15 +0900

    itemsテーブルへの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,32 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index; end
 
+  def new
+    @item = Item.new
+  end
 
-def create
-  Item.create(item_params)
-end
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    Item.create(item_params)
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :content, :category_id, :status_id, :fee_id, :area_id, :days_id, :price, :user, ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :content, :category_id, :status_id, :fee_id, :area_id, :day_id, :price, :user, ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,23 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :area
+  belongs_to_active_hash :category
+  belongs_to_active_hash :day
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :status
   belongs_to :user
   has_one :buy
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :category_id, numericality: { other_than: 0 } 
+    validates :status_id, numericality: { other_than: 0 } 
+    validates :fee_id, numericality: { other_than: 0 } 
+    validates :area_id, numericality: { other_than: 0 } 
+    validates :day_id, numericality: { other_than: 0 } 
+    validates :price
+  end
 
 end
