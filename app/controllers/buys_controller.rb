7643edb86c6d1b93a840c42c8909b10b3a189eb8
shtsukada <st01251988@gmail.com>
2020-09-04 11:13:57 +0900

    購入画面にて購入内容の表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,9 +1,19 @@
 class BuysController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+#    @item = Item.find(item_params)
   end
 
   def create
+#    @item = Item.find(item_params)
   end
+
+  # private
+
+  # def item_params
+  #   params.require(:item).permit(:image, :name, :content, :category_id, :status_id, :fee_id, :area_id, :day_id, :price, :user).merge(user_id: current_user.id)
+  # end
+
 
 end
